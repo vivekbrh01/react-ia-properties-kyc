accb9f54285dd02728b4628be32a204d479cabfa
Vivek Baruah <vivekbrh01@gmail.com>
2023-04-03 21:32:53 +0530

    Add Ekyc

--- src/components/AddKyc/AddKyc.js
+++ src/components/AddKyc/AddKyc.js
@@ -4,9 +4,10 @@ import RegistrationForm from "../RegistrationForm";
 
 import "./AddKyc.css";
 import "../../App.css";
+import Ekyc from "../Ekyc/Ekyc";
 
 function AddKyc() {
-	const [pageCount, setPageCount] = useState(0);
+	const [pageCount, setPageCount] = useState(1);
 
 	return (
 		<div className="sign-up-wrapper">
@@ -15,6 +16,7 @@ function AddKyc() {
 			) : (
 				""
 			)}
+			{pageCount === 1 ? <Ekyc setPageCount={setPageCount} /> : ""}
 		</div>
 	);
 }
